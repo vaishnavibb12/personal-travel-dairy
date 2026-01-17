b03a1f08a7b3d44944d88350bc2b5c4529182701
vaishnavi <vaishabiramane@gmail.com>
2026-01-17 11:57:42 +0530

    reactrouter install

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Auth/Home/Home";
+import SignUp from "./pages/Auth/SignUp";
+import Login from "./pages/Auth/Login";
 
 const App = () => {
-  return <div className="text text-2xl">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/sign-up" exact element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
