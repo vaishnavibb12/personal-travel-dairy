5013b73fa2357abb798536e8139b8dd97587429a
vaishnavi <vaishabiramane@gmail.com>
2026-01-17 17:13:11 +0530

    signup signin added

---
+++ bakend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+app.use("/api/auth", authRoutes);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
